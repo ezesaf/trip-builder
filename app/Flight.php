4a0396a081170e0cabf2244f2ea53056daf2e32d
= <=>
2016-12-06 23:27:05 -0500

    set up models fillable fields and relationships

--- app/Flight.php
+++ app/Flight.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Flight extends Model
 {
-    //
+    public $timestamps = false;
+    
+    protected $fillable = [
+        'flight_number',
+        'airline_code',
+        'airport_departure_id',
+        'airport_destination_id',
+    ];
 }
--- app/Trip.php
+++ app/Trip.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Trip extends Model
 {
-    //
+    public $timestamps = false;
+
+    /**
+     * 
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function flights()
+    {
+        return $this->hasMany(Flight::class);
+    }
 }
