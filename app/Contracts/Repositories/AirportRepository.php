bb3b6f03030efdf2fc7b90bf74f2f82dd183785d
= <=>
2016-12-06 23:31:41 -0500

    added method definition to paginate results

--- app/Contracts/Repositories/AirportRepository.php
+++ app/Contracts/Repositories/AirportRepository.php
@@ -5,11 +5,18 @@ namespace App\Contracts\Repositories;
 interface AirportRepository
 {
     /**
-     * returns a list of airports ordered alphabetically
-     * 
+     * Returns a list of airports ordered alphabetically
      * @param array $columns
      * @return array
      */
     public function getAllAirportsAlphabetically($columns = ['*']);
+
+
+    /**
+     * Returns a paginated result of airports ordered alphabetically
+     * @return array
+     */
+    public function getPaginatedAirportsAlphabetically($perPage = 30, $columns = ['*']);
+
 
 }
